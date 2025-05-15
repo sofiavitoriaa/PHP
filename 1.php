b402b1487c22066f9de43905e348802688b91b16
sofiavitoriaa <sofiavitoria4331@gmail.com>
2025-05-15 19:52:43 -0300

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../HTML/Imagens/cursoemvideo.jpg" type="image/x-icon">
+    <title> Exemplo PHP </title>
+</head>
+<body>
+    <h1> Exemplo PHP </h1>
+    <?php 
+        $nome = "Sofia Vitória";
+        echo "<p> O meu nome é $nome </p>";
+    ?>
+</body>
+</html>
