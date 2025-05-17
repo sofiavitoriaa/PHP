6f7bec694caa3b7e664506ba39ca9f54f87996c1
sofiavitoriaa <sofiavitoria4331@gmail.com>
2025-05-17 17:38:35 -0300

    Update cad.php

--- exercicio 4/cad.php
+++ exercicio 4/cad.php
@@ -13,8 +13,8 @@
     </header>
     <main>
         <?php 
-            $n = $_GET["nome"];
-            $s = $_GET["sobrenome"];
+            $n = $_GET["nome"] ?? "sem nome";
+            $s = $_GET["sobrenome"] ?? "desconhecido(a)";
             echo "<p> É um prazer te conhecer, <strong>$n $s</strong>! Este é o meu site!";
         ?>
         <p> <a href="javascript:history.go(-1)"> Voltar para a página anterior </a> </p>
