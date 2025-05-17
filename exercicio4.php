2626217122526dbd3f9582787f5ba46a3daa092b
sofiavitoriaa <sofiavitoria4331@gmail.com>
2025-05-17 17:01:13 -0300

    Update exercicio4.php

--- exercicio4.php
+++ exercicio4.php
@@ -12,7 +12,7 @@
         <h1> Apresente-se para nós </h1>
     </header>
     <section>
-        <form action="cad.php" method="post"> 
+        <form action="cad.php" method="get"> 
             <label for="nome"> Nome </label>
             <input type="text" name="nome" id="idnome">
             <label for="sobrenome"> Sobrenome </label>
