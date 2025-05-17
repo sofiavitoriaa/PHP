ca59e4b267a5641c40d63c34ab102645b60ad9b8
sofiavitoriaa <sofiavitoria4331@gmail.com>
2025-05-17 11:45:39 -0300

    Update strings.php

--- strings.php
+++ strings.php
@@ -43,6 +43,18 @@
         $s = "Guanabara";
         $a = "Gafanhoto";
         echo "$n \"$a\" $s";
+
+        echo "<br>";
+
+        $canal = "Curso em Vídeo";
+        $ano = date('Y');
+        echo <<< TESTE
+            Olá galera do $canal!
+                Tudo bem com vocês?
+                Como Está sendo esse ano de $ano?
+            Abraços! \u{1F596}
+        TESTE;
+
     ?>
 </body>
 </html>
